d80fc71f70525dd549c90bc325b860d677e2a4fe
Diego Rosa Castilho <diegorc@br.ibm.com>
2021-02-13 18:23:22 -0300

    'first'

---
+++ portfolio/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+
+from .models import Project 
+
+# Create your views here.
+
+def home(request):
+    
+    projects = Project.objects.all()
+
+    return render(request, 'portfolio/home.html', {'projects':projects}) 
